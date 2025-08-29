98872a3a1b3b0d23e5faa35ff71220f4c21bb7b6
yuhao <yuhao@ruli.com>
2025-08-29 09:55:14 +0800

    动态配置

--- src/Console/ChangeEnv.php
+++ src/Console/ChangeEnv.php
@@ -4,7 +4,7 @@ namespace XiaoyJayUs\console;
 
 
 use think\Console\Command;
-use think\Console\input\Argument;
+use think\Console\input\Option;
 
 class ChangeEnv extends Command
 {
@@ -14,59 +14,39 @@ class ChangeEnv extends Command
 
     protected function configure()
     {
+        $configNmaes = array_keys(config('change_env'));
         $this->setName("change:env")
-            ->addArgument('mysql', Argument::OPTIONAL, 'local/dev/prod', 'local')
-            ->addArgument('mongo', Argument::OPTIONAL, 'local/dev/prod', 'local')
-            ->addArgument('redis', Argument::OPTIONAL, 'local/dev/prod', 'local')
             ->setDescription('开发环境切换');
+        foreach ($configNmaes as $name) {
+            $this->addOption($name, null, Option::VALUE_OPTIONAL, 'local/dev/prod', 'local');
+        }
     }
 
     public function handle(): bool
     {
         $this->config = $this->app->config->get('change_env');
-        $mysql        = $this->input->getArgument('mysql');
-        $mongo        = $this->input->getArgument('mongo');
-        $redis        = $this->input->getArgument('redis');
-
-        if (empty($this->config['mysql'][$mysql])) {
-            $this->output->error('mysql配置不存在');
-            exit;
-        } elseif (empty($this->config['redis'][$redis])) {
-            $this->output->error('redis配置不存在');
-            exit;
-        }
-        $mysqlConfig = $this->config['mysql'][$mysql];
-        $redisConfig = $this->config['redis'][$redis];
-        $mongoConfig = $this->config['mongo'][$mongo];
-        $file        = $this->app->getRootPath() . '.env';
-        $content     = file_get_contents($file);
-        $content     = preg_replace("/\r/", PHP_EOL, $content);
-
-        # 替换 mysql 配置
-        foreach ($mysqlConfig as $key => $value) {
-            $pattern     = '/^' . preg_quote($key, '/') . '=.*$/m';
-            $replacement = $key . '=' . $value;
-            $content     = preg_replace($pattern, $replacement, $content);
-        }
-
-        # 替换 mongo 配置
-        foreach ($mongoConfig as $key => $value) {
-            $pattern     = '/^' . preg_quote($key, '/') . '=.*$/m';
-            $replacement = $key . '=' . $value;
-            $content     = preg_replace($pattern, $replacement, $content);
-        }
-
-        # 替换 redis 配置
-        foreach ($redisConfig as $key => $value) {
-            $pattern     = '/^' . preg_quote($key, '/') . '=.*$/m';
-            $replacement = $key . '=' . $value;
-            $content     = preg_replace($pattern, $replacement, $content);
+        $configNmaes  = array_keys($this->config);
+        $options      = $this->input->getOptions();
+        $file         = $this->app->getRootPath() . '.env';
+        $content      = file_get_contents($file);
+        $content      = preg_replace("/\r/", PHP_EOL, $content);
+        $msg          = [];
+        foreach ($configNmaes as $name) {
+            $inputEnv  = $options[$name];
+            $nowConfig = $this->config[$name][$inputEnv];
+            # 替换配置
+            foreach ($nowConfig as $key => $value) {
+                $pattern     = '/^' . preg_quote($key, '/') . '=.*$/m';
+                $replacement = $key . '=' . $value;
+                $content     = preg_replace($pattern, $replacement, $content);
+            }
+            $msg[] = "{$name}:{$inputEnv}";
         }
 
         #替换源文件
         file_put_contents($file, $content);
 
-        $this->output->info("mysql:{$mysql}\nmongo:{$mongo}\nredis:{$redis}");
+        $this->output->info(implode(PHP_EOL, $msg));
         return true;
     }
 }
